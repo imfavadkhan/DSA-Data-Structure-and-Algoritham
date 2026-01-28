661736cc88f6b969a7bff4a82d4a8ca027330496
Fawad Khan <imfavadkhan@gmail.com>
2026-01-28 23:38:11 +0500

    Add files via upload

---
+++ Recursion/tayler-series.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+double e(int x, int n)
+{
+    static double p=1, f=1;
+    double r;
+    if(n==0)
+    {
+        return 1;
+    }
+    else
+    {
+        r=e(x,n-1);
+        p=p*x;
+        f=f*n;
+        return r+p/f;
+    }
+
+}
+
+int main()
+{
+    std::cout<<e(2,15)<<std::endl;
+
+    return 0;
+}
